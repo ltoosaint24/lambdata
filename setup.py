20cec1667fd81b16904a1712deb43b98a87fd568
LToussaint <67988647+ltoosaint24@users.noreply.github.com>
2021-03-20 08:39:29 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,11 +8,15 @@ setup(
   author = 'Loveline toussaint',                   # Type in your name
   author_email = 'ltoosaint24@outlook.com',      # Type in your E-Mail
   url = 'https://github.com/ltoosaint24/lambdata-ltoussaint-',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/ltoosaint24/lambdata-ltoussaint-/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/ltoosaint24/lambdata-ltoussaint-/archive/refs/tags/v_01.tar.gz',    # I explain this later on
   keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS','Fib', 'State', 'Abbreviation','Function','Transpose'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'validators',
           'beautifulsoup4',
+          'numpy,
+          'pandas',
+          'random',
+          'scrapeasy'
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
