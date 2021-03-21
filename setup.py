6a2460f8c1a9d170f5481ae1d8e34b10c5bba7b9
LToussaint <67988647+ltoosaint24@users.noreply.github.com>
2021-03-21 09:48:14 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'mypackage',         # How you named your package folder (MyLib)
   packages = ['mypackage'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '0.112',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'A initial starting Helper function package created by Loveline Toussaint. It consist of a fib statement, state abbreviation module,and dataframe transpose module',   # Give a short description about your library
   author = 'Loveline toussaint',                   # Type in your name
   author_email = 'ltoosaint24@outlook.com',      # Type in your E-Mail
   url = 'https://github.com/ltoosaint24/lambdata-ltoussaint-',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/ltoosaint24/lambdata-ltoussaint-/archive/refs/tags/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/ltoosaint24/lambdata-ltoussaint-/archive/refs/tags/v_01.1.tar.gz',    # I explain this later on
   keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS','Fib', 'State', 'Abbreviation','Function','Transpose'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'validators',
