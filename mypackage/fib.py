fb3bfb011df58f0a8c795763630fec1417da26c6
LToussaint <67988647+ltoosaint24@users.noreply.github.com>
2021-03-20 22:21:33 -0400

    Create fib.py

---
+++ mypackage/fib.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as numy
+import random
+import scrapeasy
+
+def fib(n):
+    a,b = 0,1
+    while a< n:
+        print(a, end= ' ')
+        a, b = b, a+b
+    print()
+ 
+ 
+def fib2(n):
+	result = []
+	a,b = 0,1
+	while a<n:
+		result.append(a)
+		a,b = b, a+b
+	return result
